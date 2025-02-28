581c30c7b7bbf0690f540e95c5b65203ed82261c
J1w0n-H <jhwang97@terpmail.umd.edu>
2025-02-28 16:05:45 -0500

    ogimage

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://j1w0n-log.vercel.app",
   since: 2025, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://og-image-korean.vercel.app/J1w0n.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fmorethan-log.vercel.app%2Favatar.svg", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
