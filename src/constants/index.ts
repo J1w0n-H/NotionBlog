5614339b73647ac4bd6b5bbb816044e07e3df389
Jiwon Hwang <jhwang97@terpmail.umd.edu>
2025-08-27 11:52:07 -0400

    fix: queryKey import 에러 수정 및 useCallback 최적화

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_CATEGORY = "📂 All" as string
+export { queryKey } from "./queryKey"
--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,6 +1,6 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { getCookie, setCookie } from "cookies-next"
-import { useEffect } from "react"
+import { useEffect, useCallback } from "react"
 import { queryKey } from "src/constants"
 
 export type LanguageType = "ko" | "en"
@@ -16,10 +16,10 @@ const useLanguage = (): [LanguageType, SetLanguage] => {
     initialData: "ko" as LanguageType, // 기본값을 한국어로 설정
   })
 
-  const setLanguage = (language: LanguageType) => {
+  const setLanguage = useCallback((language: LanguageType) => {
     setCookie("language", language)
     queryClient.setQueryData(queryKey.language(), language)
-  }
+  }, [queryClient])
 
   useEffect(() => {
     if (typeof window === "undefined") return
